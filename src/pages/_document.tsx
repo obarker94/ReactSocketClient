e7ee47bd072861739caff46b5427d5470b712636
obarker94 <me@oliverbarker.io>
2023-01-06 18:08:04 +0000

    poppins font added

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="stylesheet preload prefetch"
+          href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700&display=swap"
+          as="style"
+        />
+      </Head>
+      <Main />
+      <NextScript />
+    </Html>
+  );
+}
